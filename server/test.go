fcaebb9e4115dcd4b5ad0c67539b2a77fc742373
Xavi Flores <xa_franc@live.com>
2025-03-21 15:57:34 -0600

    stream server

--- server/test.go
+++ server/test.go
@@ -3,9 +3,11 @@ package server
 import (
 	"context"
 	"io"
+	"time"
 
 	"github.com/xavicci/rsg1/models"
 	"github.com/xavicci/rsg1/repository"
+	"github.com/xavicci/rsg1/studentpb"
 	"github.com/xavicci/rsg1/testpb"
 )
 
@@ -64,4 +66,49 @@ func (s *TestServer) SetQuestion(stream testpb.TestService_SetQuestionServer) er
 			return stream.SendAndClose(&testpb.SetQuestionResponse{Ok: false})
 		}
 	}
+}
+
+func (s *TestServer) EnrollStudents(stream testpb.TestService_EnrollStudentsServer) error {
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&testpb.SetQuestionResponse{
+				Ok: true,
+			})
+		}
+		if err != nil {
+			return err
+		}
+
+		enrollment := &models.Enrollment{
+			StudentId: msg.GetStudentId(),
+			TestId:    msg.GetTestId(),
+		}
+		err = s.repo.SetEnrollment(context.Background(), enrollment)
+		if err != nil {
+			return stream.SendAndClose(&testpb.SetQuestionResponse{
+				Ok: false,
+			})
+		}
+	}
+}
+
+func (s *TestServer) GetStudentsPerTest(req *testpb.GetStudentsPerTestRequest, stream testpb.TestService_GetStudentsPerTestServer) error {
+	students, err := s.repo.GetStudentsPerTest(context.Background(), req.GetTestId())
+	if err != nil {
+		return err
+	}
+	for _, student := range students {
+		student := &studentpb.Student{
+			Id:   student.Id,
+			Name: student.Name,
+			Age:  student.Age,
+		}
+		err = stream.Send(student)
+		time.Sleep(time.Second * 2)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
